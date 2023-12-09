2c3371705b0f6030d63b3243b1d595bfdde8dd6d
James Atkin <curlyw42@gmail.com>
2023-12-09 13:48:28 +0000

    add tests for accident stats (#2)
    * add tests for accident stats
    
    * check error

--- accident_stats_test.go
+++ accident_stats_test.go
@@ -2,6 +2,9 @@ package tfl_test
 
 import (
 	"context"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/jamesalexatkin/tfl-go"
@@ -12,24 +15,58 @@ func Test_GetAccidentDetails(t *testing.T) {
 	t.Parallel()
 
 	tests := []struct {
-		name    string
-		c       *tfl.Client
-		year    int
-		want    []tfl.AccidentDetail
-		wantErr bool
-	}{}
-	for _, tt := range tests {
-		tt := tt
-		t.Run(tt.name, func(t *testing.T) {
+		name        string
+		year        int
+		testServer  *httptest.Server
+		expected    []tfl.AccidentDetail
+		expectedErr bool
+	}{
+		{
+			name: "Success",
+			testServer: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				accidentDetails := []tfl.AccidentDetail{
+					{ID: 123},
+				}
+				bytes, err := json.Marshal(accidentDetails)
+				require.NoError(t, err)
+				w.WriteHeader(http.StatusOK)
+				_, err = w.Write(bytes)
+				require.NoError(t, err)
+			})),
+			expected: []tfl.AccidentDetail{
+				{ID: 123},
+			},
+		},
+		{
+			name: "Fail on non-200 response code",
+			testServer: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusForbidden)
+			})),
+			expected:    nil,
+			expectedErr: true,
+		},
+	}
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
 
-			got, err := tt.c.GetAccidentDetails(context.Background(), tt.year)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("Client.GetAccidentDetails() error = %v, wantErr %v", err, tt.wantErr)
+			defer tc.testServer.Close()
+
+			c := tfl.Client{
+				AppID:      "app_id",
+				AppKey:     "app_key",
+				APIBaseURL: tc.testServer.URL,
+				HTTPClient: &http.Client{},
+			}
+
+			actual, err := c.GetAccidentDetails(context.Background(), tc.year)
+			if (err != nil) != tc.expectedErr {
+				t.Errorf("Client.GetAccidentDetails() error = %v, wantErr %v", err, tc.expectedErr)
 
 				return
 			}
-			require.Equal(t, tt.want, got)
+			require.Equal(t, tc.expected, actual)
 		})
 	}
 }
