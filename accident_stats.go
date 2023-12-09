594098121899beb4d781c84ac0b59d749b19241e
James Atkin <curlyw42@gmail.com>
2023-12-09 13:21:39 +0000

    reduce some long lines

--- accident_stats.go
+++ accident_stats.go
@@ -6,6 +6,7 @@ import (
 )
 
 // GetAccidentDetails gets all accident details for accidents occurring in the specified year.
+//
 // https://api.tfl.gov.uk/swagger/ui/index.html?url=/swagger/docs/v1#!/AccidentStats/AccidentStats_Get
 func (c *Client) GetAccidentDetails(ctx context.Context, year int) ([]AccidentDetail, error) {
 	path := fmt.Sprintf("/AccidentStats/%d", year)
--- mode.go
+++ mode.go
@@ -6,6 +6,7 @@ import (
 )
 
 // GetActiveServiceTypes returns the service type active for a mode. Currently only supports tube.
+//
 // https://api.tfl.gov.uk/swagger/ui/index.html?url=/swagger/docs/v1#!/Mode/Mode_GetActiveServiceTypes
 func (c *Client) GetActiveServiceTypes(ctx context.Context) ([]ActiveServiceType, error) {
 	path := "/Mode/ActiveServiceTypes"
@@ -20,6 +21,7 @@ func (c *Client) GetActiveServiceTypes(ctx context.Context) ([]ActiveServiceType
 }
 
 // GetArrivalPredictionsForMode gets the next arrival predictions for all stops of a given mode.
+//
 // https://api.tfl.gov.uk/swagger/ui/index.html?url=/swagger/docs/v1#!/Mode/Mode_Arrivals
 func (c *Client) GetArrivalPredictionsForMode(ctx context.Context, mode string, count int) ([]Prediction, error) {
 	path := fmt.Sprintf("/Mode/%s/Arrivals", mode)
