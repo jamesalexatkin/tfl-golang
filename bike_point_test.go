6a1886c87666d87936fe8d7e495bc201009b11ad
James Atkin <curlyw42@gmail.com>
2023-12-30 11:16:47 +0000

    add tests for bike point (#9)
    * add new internal test package
    
    * add first bike points integration tests
    
    * add integration tests
    
    * add unit tests
    
    * fix some lint issues

---
+++ bike_point_test.go
@@ -0,0 +1,265 @@
+package tfl_test
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/jamesalexatkin/tfl-go"
+	"github.com/jamesalexatkin/tfl-go/internal/test"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_GetAllBikePoints(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name        string
+		testServer  *httptest.Server
+		expected    []tfl.Place
+		expectedErr bool
+	}{
+		{
+			name: "Success",
+			testServer: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				bikePoints := []tfl.Place{
+					{ID: "BikePoint_1", CommonName: "Cycle Street"},
+				}
+				bytes, err := json.Marshal(bikePoints)
+				require.NoError(t, err)
+				w.WriteHeader(http.StatusOK)
+				_, err = w.Write(bytes)
+				require.NoError(t, err)
+			})),
+			expected: []tfl.Place{
+				{ID: "BikePoint_1", CommonName: "Cycle Street"},
+			},
+		},
+		{
+			name: "Fail on non-200 response code",
+			testServer: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusForbidden)
+			})),
+			expected:    nil,
+			expectedErr: true,
+		},
+	}
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			defer tc.testServer.Close()
+
+			c := tfl.Client{
+				AppID:      "app_id",
+				AppKey:     "app_key",
+				APIBaseURL: tc.testServer.URL,
+				HTTPClient: &http.Client{},
+			}
+
+			actual, err := c.GetAllBikePoints(context.Background())
+			if (err != nil) != tc.expectedErr {
+				t.Errorf("Client.GetAccidentDetails() error = %v, wantErr %v", err, tc.expectedErr)
+
+				return
+			}
+			require.Equal(t, tc.expected, actual)
+		})
+	}
+}
+
+func Test_GetAllBikePointsIntegration(t *testing.T) {
+	t.Parallel()
+
+	test.SkipIfShort(t)
+
+	api := test.NewTestClient()
+
+	bikePoints, err := api.GetAllBikePoints(context.Background())
+	require.NoError(t, err)
+
+	expectedBikePoint := &tfl.Place{
+		ID:           "BikePoints_1",
+		URL:          "/Place/BikePoints_1",
+		CommonName:   "River Street , Clerkenwell",
+		Distance:     0,
+		PlaceType:    "BikePoint",
+		Children:     []tfl.Place{},
+		ChildrenURLs: []string{},
+		Lat:          51.529163,
+		Lon:          -0.10997,
+	}
+
+	test.RequireBikePointsEqual(t, expectedBikePoint, &bikePoints[0])
+}
+
+func Test_GetBikePoint(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name        string
+		testServer  *httptest.Server
+		bikePointID string
+		expected    *tfl.Place
+		expectedErr bool
+	}{
+		{
+			name: "Success",
+			testServer: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				bikePoint := &tfl.Place{ID: "BikePoint_1", CommonName: "Cycle Street"}
+				bytes, err := json.Marshal(bikePoint)
+				require.NoError(t, err)
+				w.WriteHeader(http.StatusOK)
+				_, err = w.Write(bytes)
+				require.NoError(t, err)
+			})),
+			bikePointID: "BikePoint_1",
+			expected:    &tfl.Place{ID: "BikePoint_1", CommonName: "Cycle Street"},
+		},
+		{
+			name: "Fail on non-200 response code",
+			testServer: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusForbidden)
+			})),
+			expected:    nil,
+			expectedErr: true,
+		},
+	}
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			defer tc.testServer.Close()
+
+			c := tfl.Client{
+				AppID:      "app_id",
+				AppKey:     "app_key",
+				APIBaseURL: tc.testServer.URL,
+				HTTPClient: &http.Client{},
+			}
+
+			actual, err := c.GetBikePoint(context.Background(), tc.bikePointID)
+			if (err != nil) != tc.expectedErr {
+				t.Errorf("Client.GetAccidentDetails() error = %v, wantErr %v", err, tc.expectedErr)
+
+				return
+			}
+			require.Equal(t, tc.expected, actual)
+		})
+	}
+}
+
+func Test_GetBikePointIntegration(t *testing.T) {
+	t.Parallel()
+
+	test.SkipIfShort(t)
+
+	api := test.NewTestClient()
+
+	bikePoint, err := api.GetBikePoint(context.Background(), "BikePoints_1")
+	require.NoError(t, err)
+
+	expectedBikePoint := &tfl.Place{
+		ID:           "BikePoints_1",
+		URL:          "/Place/BikePoints_1",
+		CommonName:   "River Street , Clerkenwell",
+		Distance:     0,
+		PlaceType:    "BikePoint",
+		Children:     []tfl.Place{},
+		ChildrenURLs: nil,
+		Lat:          51.529163,
+		Lon:          -0.10997,
+	}
+
+	test.RequireBikePointsEqual(t, expectedBikePoint, bikePoint)
+}
+
+func Test_SearchBikePoint(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name        string
+		testServer  *httptest.Server
+		searchQuery string
+		expected    []tfl.Place
+		expectedErr bool
+	}{
+		{
+			name: "Success",
+			testServer: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				bikePoint := []tfl.Place{
+					{ID: "BikePoint_1", CommonName: "Cycle Street"},
+				}
+				bytes, err := json.Marshal(bikePoint)
+				require.NoError(t, err)
+				w.WriteHeader(http.StatusOK)
+				_, err = w.Write(bytes)
+				require.NoError(t, err)
+			})),
+			searchQuery: "Cycle Street",
+			expected: []tfl.Place{
+				{ID: "BikePoint_1", CommonName: "Cycle Street"},
+			},
+		},
+		{
+			name: "Fail on non-200 response code",
+			testServer: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusForbidden)
+			})),
+			expected:    nil,
+			expectedErr: true,
+		},
+	}
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			defer tc.testServer.Close()
+
+			c := tfl.Client{
+				AppID:      "app_id",
+				AppKey:     "app_key",
+				APIBaseURL: tc.testServer.URL,
+				HTTPClient: &http.Client{},
+			}
+
+			actual, err := c.SearchBikePoint(context.Background(), tc.searchQuery)
+			if (err != nil) != tc.expectedErr {
+				t.Errorf("Client.GetAccidentDetails() error = %v, wantErr %v", err, tc.expectedErr)
+
+				return
+			}
+			require.Equal(t, tc.expected, actual)
+		})
+	}
+}
+
+func Test_SearchBikePointIntegration(t *testing.T) {
+	t.Parallel()
+
+	test.SkipIfShort(t)
+
+	api := test.NewTestClient()
+
+	bikePoints, err := api.SearchBikePoint(context.Background(), "River Street")
+	require.NoError(t, err)
+
+	expectedBikePoint := &tfl.Place{
+		ID:           "BikePoints_1",
+		URL:          "/Place/BikePoints_1",
+		CommonName:   "River Street , Clerkenwell",
+		Distance:     0,
+		PlaceType:    "BikePoint",
+		Children:     []tfl.Place{},
+		ChildrenURLs: []string{},
+		Lat:          51.529163,
+		Lon:          -0.10997,
+	}
+
+	test.RequireBikePointsEqual(t, expectedBikePoint, &bikePoints[0])
+}
---
+++ internal/test/test.go
@@ -0,0 +1,45 @@
+package test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/jamesalexatkin/tfl-go"
+	"github.com/stretchr/testify/require"
+)
+
+const latLonEpsilon = 0.005
+
+// SkipIfShort can be used to skip a test if the tests are run with the `-short` flag.
+func SkipIfShort(t *testing.T) {
+	t.Helper()
+
+	if testing.Short() {
+		t.Skip()
+	}
+}
+
+// NewTestClient creates a new client for integration tests.
+func NewTestClient() *tfl.Client {
+	appID := os.Getenv("APP_ID")
+	appKey := os.Getenv("APP_KEY")
+
+	return tfl.New(appID, appKey)
+}
+
+// RequireBikePointsEqual ensures that two bike points are equal.
+// This uses a shallow comparison, leaving out additional properties which have modification timestamps
+// and would cause tests to be more brittle.
+func RequireBikePointsEqual(t *testing.T, expectedBikePoint *tfl.Place, bikePoint *tfl.Place) {
+	t.Helper()
+
+	require.Equal(t, expectedBikePoint.ID, bikePoint.ID)
+	require.Equal(t, expectedBikePoint.URL, bikePoint.URL)
+	require.Equal(t, expectedBikePoint.CommonName, bikePoint.CommonName)
+	require.Equal(t, expectedBikePoint.Distance, bikePoint.Distance)
+	require.Equal(t, expectedBikePoint.PlaceType, bikePoint.PlaceType)
+	require.Equal(t, expectedBikePoint.Children, bikePoint.Children)
+	require.Equal(t, expectedBikePoint.ChildrenURLs, bikePoint.ChildrenURLs)
+	require.InEpsilon(t, expectedBikePoint.Lat, bikePoint.Lat, latLonEpsilon)
+	require.InEpsilon(t, expectedBikePoint.Lon, bikePoint.Lon, latLonEpsilon)
+}
