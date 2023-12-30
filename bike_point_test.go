e9475ddd8cb89047ee9ffd956947decc53a55530
James Atkin <curlyw42@gmail.com>
2023-12-30 11:05:22 +0000

    add unit tests

--- bike_point_test.go
+++ bike_point_test.go
@@ -2,6 +2,9 @@ package tfl_test
 
 import (
 	"context"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/jamesalexatkin/tfl-go"
@@ -10,9 +13,67 @@ import (
 )
 
 // TODO:
-// GetAll
 // Get
 // Search
+
+func Test_GetAllBikePoints(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name        string
+		testServer  *httptest.Server
+		expected    []tfl.Place
+		expectedErr bool
+	}{
+		{
+			name: "Success",
+			testServer: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				bikePoints := []tfl.Place{
+					{ID: "BikePoint_1", CommonName: "Cycle Street"},
+				}
+				bytes, err := json.Marshal(bikePoints)
+				require.NoError(t, err)
+				w.WriteHeader(http.StatusOK)
+				_, err = w.Write(bytes)
+				require.NoError(t, err)
+			})),
+			expected: []tfl.Place{
+				{ID: "BikePoint_1", CommonName: "Cycle Street"},
+			},
+		},
+		{
+			name: "Fail on non-200 response code",
+			testServer: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusForbidden)
+			})),
+			expected:    nil,
+			expectedErr: true,
+		},
+	}
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			defer tc.testServer.Close()
+
+			c := tfl.Client{
+				AppID:      "app_id",
+				AppKey:     "app_key",
+				APIBaseURL: tc.testServer.URL,
+				HTTPClient: &http.Client{},
+			}
+
+			actual, err := c.GetAllBikePoints(context.Background())
+			if (err != nil) != tc.expectedErr {
+				t.Errorf("Client.GetAccidentDetails() error = %v, wantErr %v", err, tc.expectedErr)
+
+				return
+			}
+			require.Equal(t, tc.expected, actual)
+		})
+	}
+}
 
 func Test_GetAllBikePointsIntegration(t *testing.T) {
 	t.Parallel()
@@ -39,6 +100,63 @@ func Test_GetAllBikePointsIntegration(t *testing.T) {
 	test.RequireBikePointsEqual(t, expectedBikePoint, &bikePoints[0])
 }
 
+func Test_GetBikePoint(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name        string
+		testServer  *httptest.Server
+		bikePointID string
+		expected    *tfl.Place
+		expectedErr bool
+	}{
+		{
+			name: "Success",
+			testServer: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				bikePoint := &tfl.Place{ID: "BikePoint_1", CommonName: "Cycle Street"}
+				bytes, err := json.Marshal(bikePoint)
+				require.NoError(t, err)
+				w.WriteHeader(http.StatusOK)
+				_, err = w.Write(bytes)
+				require.NoError(t, err)
+			})),
+			bikePointID: "BikePoint_1",
+			expected:    &tfl.Place{ID: "BikePoint_1", CommonName: "Cycle Street"},
+		},
+		{
+			name: "Fail on non-200 response code",
+			testServer: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusForbidden)
+			})),
+			expected:    nil,
+			expectedErr: true,
+		},
+	}
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			defer tc.testServer.Close()
+
+			c := tfl.Client{
+				AppID:      "app_id",
+				AppKey:     "app_key",
+				APIBaseURL: tc.testServer.URL,
+				HTTPClient: &http.Client{},
+			}
+
+			actual, err := c.GetBikePoint(context.Background(), tc.bikePointID)
+			if (err != nil) != tc.expectedErr {
+				t.Errorf("Client.GetAccidentDetails() error = %v, wantErr %v", err, tc.expectedErr)
+
+				return
+			}
+			require.Equal(t, tc.expected, actual)
+		})
+	}
+}
+
 func Test_GetBikePointIntegration(t *testing.T) {
 	t.Parallel()
 
@@ -62,6 +180,67 @@ func Test_GetBikePointIntegration(t *testing.T) {
 	}
 
 	test.RequireBikePointsEqual(t, expectedBikePoint, bikePoint)
+}
+
+func Test_SearchBikePoint(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name        string
+		testServer  *httptest.Server
+		searchQuery string
+		expected    []tfl.Place
+		expectedErr bool
+	}{
+		{
+			name: "Success",
+			testServer: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				bikePoint := []tfl.Place{
+					{ID: "BikePoint_1", CommonName: "Cycle Street"},
+				}
+				bytes, err := json.Marshal(bikePoint)
+				require.NoError(t, err)
+				w.WriteHeader(http.StatusOK)
+				_, err = w.Write(bytes)
+				require.NoError(t, err)
+			})),
+			searchQuery: "Cycle Street",
+			expected: []tfl.Place{
+				{ID: "BikePoint_1", CommonName: "Cycle Street"},
+			},
+		},
+		{
+			name: "Fail on non-200 response code",
+			testServer: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusForbidden)
+			})),
+			expected:    nil,
+			expectedErr: true,
+		},
+	}
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			defer tc.testServer.Close()
+
+			c := tfl.Client{
+				AppID:      "app_id",
+				AppKey:     "app_key",
+				APIBaseURL: tc.testServer.URL,
+				HTTPClient: &http.Client{},
+			}
+
+			actual, err := c.SearchBikePoint(context.Background(), tc.searchQuery)
+			if (err != nil) != tc.expectedErr {
+				t.Errorf("Client.GetAccidentDetails() error = %v, wantErr %v", err, tc.expectedErr)
+
+				return
+			}
+			require.Equal(t, tc.expected, actual)
+		})
+	}
 }
 
 func Test_SearchBikePointIntegration(t *testing.T) {
